e4ac4726dc923a5da5cf7706a5ba96858488672b
Мария Томилова <tomilovam@Noutbuk-Maria.local>
2024-08-21 19:03:17 +0300

    Uploaded bin/brain-calc.js, bin/brain-even.js, bin/brain-gcd.js, eslint.config.js, src/cli.js, src/games/progression-game.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+import { FlatCompat } from "@eslint/js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname
+});
 
 export default [
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
+  ...compat.extends("standard", "example"),
 ];
